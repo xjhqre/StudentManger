bb27ad5b75989fc68f5221ec83ad8a5b5acd31b0
xjhqre <859425698@qq.com>
2021-08-22 15:08:43 +0800

    first commit

--- src/temp.java
+++ src/temp.java
@@ -4,12 +4,13 @@ public class temp {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         init();
     }
-
+    ///asfaf
     public static void init() throws IOException, ClassNotFoundException {
         File file = new File(StudentConstant.FILE);
         if (file.length() == 0) {
             return;
         }
+        //
         try (ObjectInputStream oi = new ObjectInputStream(new FileInputStream(file))) {
             Student s = null;
             while ((s = (Student) oi.readObject()) != null) {
